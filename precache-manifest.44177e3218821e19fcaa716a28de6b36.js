588693c11808c4cadf3ef7425c69dbd70c8c78cc
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-09-24 17:37:10 -0500

    Updates

---
+++ precache-manifest.44177e3218821e19fcaa716a28de6b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4732b0488b06543ea68d9fd29ae1fe8",
+    "url": "/cs-build-week-1-site/index.html"
+  },
+  {
+    "revision": "40425d0ed401a5aa848f",
+    "url": "/cs-build-week-1-site/static/css/main.6b550174.chunk.css"
+  },
+  {
+    "revision": "28ce6f94af7ab25e9f6f",
+    "url": "/cs-build-week-1-site/static/js/2.12b29505.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cs-build-week-1-site/static/js/2.12b29505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40425d0ed401a5aa848f",
+    "url": "/cs-build-week-1-site/static/js/main.7a5ff26c.chunk.js"
+  },
+  {
+    "revision": "abb4313abbf20dfd2e6b",
+    "url": "/cs-build-week-1-site/static/js/runtime-main.2277ce3c.js"
+  }
+]);
